4790affedb4eccfaf8b5fc11cf8a709b3598acd8
play21947 <play21947@gmail.com>
2023-10-31 16:23:49 +0700

    LineCanUseInRealWeb

--- LineNotify.js
+++ LineNotify.js
@@ -32,32 +32,38 @@ app.post("/webhook", (req, res) => {
     res.sendStatus(200)
 })
 
-app.post("/notify", (req, res)=>{
+app.post("/notify", async (req, res) => {
     let item_name = req.body.item_name
     let img_firebase = req.body.img_firebase
-    reply(item_name, img_firebase)
+    let res_reply = await reply(item_name, img_firebase)
+    res.json(res_reply)
 })
 
 
 const reply = (name, img) => {
-    axios.post("https://api.line.me/v2/bot/message/push", {
-        to: 'Cc516ca8f6a6d03a40eab43449d5cf8bf',
-        messages: [
-            {
-                type: 'text',
-                text: name
-            },
-            {
-                type: 'image',
-                originalContentUrl: img
+    return new Promise((resolve, reject) => {
+        axios.post("https://api.line.me/v2/bot/message/push", {
+            to: 'Cc516ca8f6a6d03a40eab43449d5cf8bf',
+            messages: [
+                {
+                    type: 'text',
+                    text: name ? 'ได้เพิ่มสินค้า : ' + name : 'ว่าง'
+                },
+                {
+                    type: 'image',
+                    originalContentUrl: img ? img : 'https://oc1t.com/upload/source/y9DpT.jpg',
+                    previewImageUrl: img ? img : 'https://oc1t.com/upload/source/y9DpT.jpg'
+                }
+            ]
+        }, {
+            headers: {
+                'Authorization': "Bearer " + env.ACCESS_TOKEN
             }
-        ]
-    }, {
-        headers: {
-            'Authorization': "Bearer " + env.ACCESS_TOKEN
-        }
-    }).then((res) => {
-        console.log(res.status)
+        }).then((res) => {
+            if (res.data) {
+                resolve({ success: true })
+            }
+        })
     })
 }
 
