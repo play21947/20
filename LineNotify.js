cc7fe3bb81f9d39192c6e6ffa5e9fe9ac945b804
play21947 <play21947@gmail.com>
2023-11-07 00:16:45 +0700

    hide Image Sending

--- LineNotify.js
+++ LineNotify.js
@@ -37,8 +37,8 @@ app.post("/webhook", (req, res) => {
 app.post("/notify", async (req, res) => {
     let item_name = req.body.item_name
     let item_remain = req.body.item_remain
-    let img_firebase = req.body.img_firebase
-    let res_reply = await reply(item_name, item_remain, img_firebase)
+    // let img_firebase = req.body.img_firebase
+    let res_reply = await reply(item_name, item_remain)
     res.json(res_reply)
 })
 
@@ -56,11 +56,11 @@ const reply = (name, item_remain ,img) => {
                     type: 'text',
                     text: item_remain ? 'คงเหลือ : '+item_remain : 'คงเหลือ : -'
                 },
-                {
-                    type: 'image',
-                    originalContentUrl: img ? img : 'https://oc1t.com/upload/source/y9DpT.jpg',
-                    previewImageUrl: img ? img : 'https://oc1t.com/upload/source/y9DpT.jpg'
-                },
+                // {
+                //     type: 'image',
+                //     originalContentUrl: img ? img : 'https://oc1t.com/upload/source/y9DpT.jpg',
+                //     previewImageUrl: img ? img : 'https://oc1t.com/upload/source/y9DpT.jpg'
+                // },
             ]
         }, {
             headers: {
@@ -77,5 +77,5 @@ const reply = (name, item_remain ,img) => {
 
 app.listen(3001, () => {
     console.log("Server is running on port 3001")
-    console.log("Teset")
+    console.log("Run on production")
 })
