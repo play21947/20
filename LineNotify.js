8af96a042f2ffc703828a684ed4de95747395e17
play21947 <play21947@gmail.com>
2023-11-12 14:25:52 +0700

    add-reply-type

--- LineNotify.js
+++ LineNotify.js
@@ -24,7 +24,7 @@ app.get("/test", (req, res) => {
 
 app.post("/webhook", (req, res) => {
     console.log(req.body.events)
-    if(req.body.events.length > 0){
+    if (req.body.events.length > 0) {
         let reply_token = req.body.events[0].source.groupId
         console.log(reply_token)
         if (req.body.events[0].type === "message") {
@@ -35,51 +35,80 @@ app.post("/webhook", (req, res) => {
 })
 
 app.post("/notify", async (req, res) => {
+    let type_notify = 1
     let item_name = req.body.item_name
     let item_remain = req.body.item_remain
     let img_firebase = req.body.img_firebase
-    let res_reply = await reply(item_name, item_remain, img_firebase)
+    let res_reply = await reply(item_name, item_remain, img_firebase, type_notify)
     res.json(res_reply)
 })
 
+// type 1 is notify added
+// type 2 is notify edit
 
-app.post("/notify-edit", async (req, res)=>{
+
+app.post("/notify-edit", async (req, res) => {
+    let type_notify = 2
     let item_name = req.body.item_name
     let item_remain = req.body.item_remain
     // let img_firebase = req.body.img_firebase
-    let res_reply = await reply(item_name, item_remain)
+    let res_reply = await reply(item_name, item_remain, '', type_notify)
     res.json(res_reply)
 })
 
 
-const reply = (name, item_remain ,img) => {
+const reply = (name, item_remain, img, type_notify) => {
     return new Promise((resolve, reject) => {
-        axios.post("https://api.line.me/v2/bot/message/push", {
-            to: 'Cc516ca8f6a6d03a40eab43449d5cf8bf',
-            messages: [
-                {
-                    type: 'text',
-                    text: name ? 'สินค้า : '+name : 'สินค้า : -'
-                },
-                {
-                    type: 'text',
-                    text: item_remain ? 'คงเหลือ : '+item_remain : 'คงเหลือ : -'
-                },
-                {
-                    type: 'image',
-                    originalContentUrl: img ? img : 'https://oc1t.com/upload/source/y9DpT.jpg',
-                    previewImageUrl: img ? img : 'https://oc1t.com/upload/source/y9DpT.jpg'
-                },
-            ]
-        }, {
-            headers: {
-                'Authorization': "Bearer " + env.ACCESS_TOKEN
-            }
-        }).then((res) => {
-            if (res.data) {
-                resolve({ success: true })
-            }
-        })
+        if (type_notify == 1) {
+            axios.post("https://api.line.me/v2/bot/message/push", {
+                to: 'Cc516ca8f6a6d03a40eab43449d5cf8bf',
+                messages: [
+                    {
+                        type: 'text',
+                        text: name ? 'สินค้า : ' + name : 'สินค้า : -'
+                    },
+                    {
+                        type: 'text',
+                        text: item_remain ? 'คงเหลือ : ' + item_remain : 'คงเหลือ : -'
+                    },
+                    {
+                        type: 'image',
+                        originalContentUrl: img ? img : 'https://oc1t.com/upload/source/y9DpT.jpg',
+                        previewImageUrl: img ? img : 'https://oc1t.com/upload/source/y9DpT.jpg'
+                    },
+                ]
+            }, {
+                headers: {
+                    'Authorization': "Bearer " + env.ACCESS_TOKEN
+                }
+            }).then((res) => {
+                if (res.data) {
+                    resolve({ success: true })
+                }
+            })
+        } else if (type_notify == 2) {
+            axios.post("https://api.line.me/v2/bot/message/push", {
+                to: 'Cc516ca8f6a6d03a40eab43449d5cf8bf',
+                messages: [
+                    {
+                        type: 'text',
+                        text: name ? 'สินค้า : ' + name : 'สินค้า : -'
+                    },
+                    {
+                        type: 'text',
+                        text: item_remain ? 'คงเหลือ : ' + item_remain : 'คงเหลือ : -'
+                    },
+                ]
+            }, {
+                headers: {
+                    'Authorization': "Bearer " + env.ACCESS_TOKEN
+                }
+            }).then((res) => {
+                if (res.data) {
+                    resolve({ success: true })
+                }
+            })
+        }
     })
 }
 
